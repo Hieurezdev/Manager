08e6dba9456e52868c675a4fce41c104c3b24244
Dat <maidat2005tbi@gmail.com>
2026-05-31 14:31:03 +0700

    giao dien

--- src/com/example/manager/dao/DBConnection.java
+++ src/com/example/manager/dao/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/quanlytauhoa?useSSL=false&serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost:3306/manager_db?useSSL=false&serverTimezone=Asia/Ho_Chi_Minh&allowPublicKeyRetrieval=true&createDatabaseIfNotExist=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASSWORD = "maitiendat190705";
+    private static final String PASSWORD = "Hch301105#";
 
     public static Connection getConnection() {
         try {
